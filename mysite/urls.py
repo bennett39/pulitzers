cea57e657f4787b99aeb01815db026f134aa7a15
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-06-05 19:01:43 -0400

    Build static version & create template to serve it

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='frontend/index.html')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
